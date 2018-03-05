895c2ffb6ec33cba29bf3578a1deb6dc51c1494b
Esin <e.esin@g.nsu.ru>
2018-03-05 23:59:56 +0600

    Trying to implement the MoveOrder: the movement from point A to point B.

--- StrategyGame/WorldController.cpp
+++ StrategyGame/WorldController.cpp
@@ -24,8 +24,22 @@ sf::Vector2f WorldController::mapToScreen(sf::Vector2u point)
 
 bool WorldController::move(GameObject& go, sf::Vector2i direction)
 {
+	
+	go.position().x = go.position().x + direction.x;
+	go.position().y = go.position().y + direction.y;
+
+	if (go.position().x == 0 && direction.x<0)
+	{
+		return false;
+	}
+	if (go.position().y == 0 && direction.y<0)
+	{
+		return false;
+	}
+
 	// TODO: check for direction length, other objects from WorldModel don't lie in that coord
 	// return true if success
+	
 	return true;
 }
 
