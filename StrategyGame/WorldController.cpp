1a174776c91dd0645e18defa919d67560519664e
Esin <e.esin@g.nsu.ru>
2018-03-11 01:01:39 +0600

    I finished ABMovement method.

--- StrategyGame/WorldController.cpp
+++ StrategyGame/WorldController.cpp
@@ -1,5 +1,6 @@
 #include "WorldController.h"
 #include "GameObject.h"
+#include "WorldRenderer.h"
 
 WorldController::WorldController(unsigned int offset_x, unsigned int offset_y, float hex_radius) :
 	offset_x(offset_x),
@@ -30,45 +31,63 @@ std::vector<sf::Vector2i> WorldController::ABMovement(sf::Vector2u StartPosition
 	direction.x = EndPosition.x;
 	direction.y = EndPosition.y;
 
-	int x0;
-	int y0;
-	int xf;
-	int yf;
-	double a;
-	double Len;
-	double MinDistance;
+	float x0;
+	float y0;
+	float xf;
+	float yf;
+	float a;
+	float Len;
+	float MinDistance;
+	float x_steper;
+	float y_steper;
+	float OneStep_x;
+	float OneStep_y;
+	float IStep;
+	float JStep;
 
-	x0 = StartPosition.x;
-	y0 = StartPosition.y;
-	xf = EndPosition.x;
-	yf = EndPosition.y;
+	x0 = 30 - (StartPosition.x % 2) * offset_x + StartPosition.y * 2 * hex_radius * cos(30 * pi / 180.f) + hex_radius * sqrt(3) / 2;
+	y0 = 30 + StartPosition.x * offset_y + hex_radius;
 
-	while (x0 != xf && y0 != yf)
+	xf = 30 - (EndPosition.x % 2) * offset_x + EndPosition.y * 2 * hex_radius * cos(30 * pi / 180.f) + hex_radius * sqrt(3) / 2;
+	yf = 30 + EndPosition.x * offset_y + hex_radius;
+
+	OneStep_x = 30 - (sf::Vector2u{ 1,1 }.x % 2) * offset_x + sf::Vector2u{1,1}.y * 2 * hex_radius * cos(30 * pi / 180.f) + hex_radius * sqrt(3) / 2;
+	OneStep_y = 30 + sf::Vector2u{1,1}.x * offset_y + hex_radius;
+
+	x_steper = x0;
+	y_steper = y0;
+
+	while (x_steper != xf && y_steper != yf)
 	{
-		MinDistance = sqrt(pow((x0 - xf), 2) + pow((y0 - yf), 2));
+		MinDistance = sqrt(pow((x_steper - xf), 2) + pow((y_steper - yf), 2));
 		for (int i = 0; i < 2; i++)
 		{
 			for (int j = 0; j < 2; j++)
 			{
 
-				sf::Vector2i IterHex = { x0 - 1 + i,y0 - 1 + j };
+				IStep = 30 - (i % 2) * offset_x + j * 2 * hex_radius * cos(30 * pi / 180.f) + hex_radius * sqrt(3) / 2;
+				JStep = 30 + i * offset_y + hex_radius;
 
-				a = (IterHex.y - y0) / (IterHex.x - x0);
-				Len = (pow(IterHex.x - (x0 + IterHex.x + a*(y0 - IterHex.y) + sqrt(pow(x0 + xf + a*(y0 - IterHex.y), 2) - 4 * (1 + a)*(x0*IterHex.x))) / (2 * (1 + a)), 2) +
-					+pow(IterHex.y - y0 + a*((x0 + IterHex.x + a*(y0 - IterHex.y) + sqrt(pow(x0 + IterHex.x + a*(y0 - IterHex.y), 2) - 4 * (1 + a)*(x0*IterHex.x))) / (2 * (1 + a))), 2));
+				sf::Vector2f IterHex = { x_steper - OneStep_x + IStep,y_steper - OneStep_y + JStep };
+
+				a = (yf - y0) / (xf - x0);
+				Len = (pow(IterHex.x - (x_steper + IterHex.x + a*(y_steper - IterHex.y) + sqrt(pow(x_steper + xf + a*(y_steper - IterHex.y), 2) - 4 * (1 + a)*(x_steper*IterHex.x))) / (2 * (1 + a)), 2) +
+					+pow(IterHex.y - y_steper + a*((x_steper + IterHex.x + a*(y_steper - IterHex.y) + sqrt(pow(x_steper + IterHex.x + a*(y_steper - IterHex.y), 2) - 4 * (1 + a)*(x_steper*IterHex.x))) / (2 * (1 + a))), 2));
 
 				if (MinDistance > Len)
 				{
 					MinDistance = Len;
-					direction.x = IterHex.x;
-					direction.y = IterHex.y;
+					direction.x = i;
+					direction.y = j;
 				}
 
 			}
 		}
+
 		StepsVector.push_back(direction);
-		x0 = x0 + direction.x;
-		y0 = y0 + direction.y;
+
+		x_steper = x_steper + 30 - (direction.x % 2) * offset_x + direction.y * 2 * hex_radius * cos(30 * pi / 180.f) + hex_radius * sqrt(3) / 2;
+		y_steper = y_steper + 30 + direction.x * offset_y + hex_radius;
 	}
 	return(StepsVector);
 }
