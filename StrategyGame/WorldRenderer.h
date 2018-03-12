f0d052fdeb0877495d1f9b124db33e320d98cc4b
Shilov-Nikolay-Jr <n.shilov@g.nsu.ru>
2018-03-13 00:51:16 +0700

    Changed the graan hexagon functions. Now there is no problems on the boundary, but it is still drawn all the time while the window is open.

--- StrategyGame/WorldRenderer.h
+++ StrategyGame/WorldRenderer.h
@@ -6,43 +6,11 @@
 class WorldRenderer
 {
 	const float pi = 3.14159265358979323846;
+
 	float radius = 20;
 	sf::RenderTarget& renderer;
 
-	std::pair<float, float> chosenHex = std::pair<float, float>(-1, -1);
-
-	std::pair<float, float> pixelToHex(float x, float y) 
-	{
-		auto result = std::pair<float, float>(-1, -1);
-		float x_offset = 2 * radius * cos(30 * pi / 180.f) * cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
-		float y_offset = 2 * radius * cos(30 * pi / 180.f) * sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
-
-		float distance = std::numeric_limits<float>::max();
-		for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++) 
-			{
-				float x_hexCenter = 30 - (i % 2) * x_offset + j * 2 * radius * cos(30 * pi / 180.f) + radius * sqrt(3) / 2;
-				float y_hexCenter = 30 + i * y_offset + radius;
-				float dx = x - x_hexCenter;
-				float dy = y - y_hexCenter;
-				float newDistance = sqrt(dx * dx + dy * dy);
-				
-				if (newDistance > radius)
-				{
-					continue;
-				}
-				if (newDistance <= distance) 
-				{
-					distance = newDistance;
-					result.first = i;
-					result.second = j;
-				}
-			}
-		}
-		return result;
-	}
-
+	sf::Vector2i Chosen_Hex;
 
 	void renderHex(float ox, float oy)
 	{
@@ -68,6 +36,7 @@ class WorldRenderer
 	void renderGreenHex(float ox, float oy)
 	{
 		sf::CircleShape hexagon(radius, 6);
+		hexagon.setOrigin(radius, radius);
 		hexagon.setPosition(ox, oy);
 		hexagon.setOutlineThickness(1);
 		hexagon.setFillColor(sf::Color::Transparent);
@@ -80,7 +49,7 @@ class WorldRenderer
 	{
 		float x_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
 		float y_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
-
+		
 		for (int i = 0; i < 10; i++)
 		{
 			for (int j = 0; j < 10; j++)
@@ -88,8 +57,13 @@ class WorldRenderer
 				renderHex(30 - (i % 2)*x_offset + j* (2 * radius*cos(30 * pi / 180.f) - 1), 30 + i*y_offset);
 			}
 		}
-		renderGreenHex(30 - ((int)chosenHex.first % 2) * x_offset + chosenHex.second * 2 * radius * cos(30 * pi / 180.f), 30 + chosenHex.first * y_offset);
+	}
+	void draw_The_ChosenHex()
+	{
+		float x_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
+		float y_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
 
+		renderGreenHex(30 - (Chosen_Hex.y % 2) * x_offset + Chosen_Hex.x * (2 * radius * cos(30 * pi / 180.f) - 1), 30 + Chosen_Hex.y * y_offset);
 	}
 
 public:
@@ -100,18 +74,34 @@ public:
 	void render()
 	{
 		renderGrid();
+		draw_The_ChosenHex();
 		for (auto& i : WorldModel::getWorldInstance()->actors)
 		{
 			//i->render(renderer);
 		}
 	}
 
-	void selectHex(float ox, float oy)
+	void Find_The_Chosen_Hex(float x, float y)
 	{
-		float x_offset = 2 * radius * cos(30 * pi / 180.f) * cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
-		float y_offset = 2 * radius * cos(30 * pi / 180.f) * sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
+		float x_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
+		float y_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
 
-		chosenHex = pixelToHex(ox, oy);
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+
+				float CurrentHex_Center_X = 30 - (i % 2) * x_offset + j * (2 * radius * cos(30 * pi / 180.f) - 1);
+				float CurrentHex_Center_Y = 30 + i * y_offset;
+
+				if ((abs(x - CurrentHex_Center_X) <= radius*cos(30 * pi / 180.f)) &&
+					(abs(y - CurrentHex_Center_Y) <= radius - abs(x - CurrentHex_Center_X) / sqrt(3)))
+				{
+					Chosen_Hex = sf::Vector2i(j, i); //Именно в таком порядке, так как j - номер столбца в матрице, сиречь координата по X.
+				}
+			}
+		}
+		return;
 	}
 
 };
