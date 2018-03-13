99acd5ebbd21618841d45c63cc4841e5c85020a2
SerafimGrubas <s.grubas@g.nsu.ru>
2018-03-13 10:35:30 +0400

    del comments in render

--- StrategyGame/Civilian.cpp
+++ StrategyGame/Civilian.cpp
@@ -8,15 +8,7 @@ class WorldController;
 
 void Civilian::render(sf::RenderTarget& r)
 {
-	//texture1.loadFromFile("Sm_bolter_hero_2.png");
-	//space.setTexture(texture1);
-	//space.setScale(sf::Vector2f(0.1, 0.1));
 
-	//WorldController wc(30, 30, 20);
-
-	//space.setPosition(wc.mapToScreen(pos));
-
-	//r.draw(space);
 }
 
 void Civilian::update()
