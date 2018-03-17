3ce9847210b8b3cd8430ef699f991993cb12ac8b
Shilov-Nikolay-Jr <n.shilov@g.nsu.ru>
2018-03-18 00:25:44 +0700

    Finite-State_Machine.cpp: fixed the selection of a hexagon and transition to o. Now it works properly. WorldRenderer.h: added a boolean private variable which helps the Machine understand whether any hexagon is active or not. Declared a method designed for returning current value of the new boolean variable. It also helps the Machine. WorldRenderer.cpp: described the new method declared in WorldRenderer.h. Fixed the Find_The_Chosen_Hex method, so now it also changes new boolean variable to "true" if the passed position lies within any of all the hexagons and vice versa: if the position does not belong to the field of our hexagons, it makes the boolean variable "false".

--- StrategyGame/WorldRenderer.h
+++ StrategyGame/WorldRenderer.h
@@ -17,6 +17,7 @@ class WorldRenderer
 	float radius = 20;
 
 	sf::Vector2i Chosen_Hex;
+	bool A_Hex_Is_Active = 0;
 	void renderHex(float ox, float oy, sf::RenderTarget& renderer)
 	{
 		sf::CircleShape Hexagon_Internal(radius - 4, 6);
@@ -61,54 +62,13 @@ class WorldRenderer
 			}
 		}
 	}
-	void draw_The_ChosenHex(sf::RenderTarget& renderer)
-	{
-		float x_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
-		float y_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
-
-		renderGreenHex(30 - (Chosen_Hex.y % 2) * x_offset + Chosen_Hex.x * (2 * radius * cos(30 * pi / 180.f) - 1), 30 + Chosen_Hex.y * y_offset, renderer);
-	}
 
 public:
 	static WorldRenderer* Get_The_Renderer_Instance();
 
-	void render_Global_Screen(sf::RenderTarget& renderer)
-	{
-		renderGrid(renderer);
-		for (auto& i : WorldModel::getWorldInstance()->actors)
-		{
-			//i->render(renderer);
-		}
-	}
-	void render_Global_Screen_With_A_Hex_Selected(sf::RenderTarget& renderer)
-	{
-		renderGrid(renderer);
-		draw_The_ChosenHex(renderer);
-		for (auto& i : WorldModel::getWorldInstance()->actors)
-		{
-			//i->render(renderer);
-		}
-	}
-	void Find_The_Chosen_Hex(float x, float y)
-	{
-		float x_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*cos(60 * pi / 180.f); //при переходе на новую строчку через раз надо будет смещать первый шестиугольник в ряду на такой шаг вправо или влево.
-		float y_offset = (2 * radius*cos(30 * pi / 180.f) - 1)*sin(60 * pi / 180.f); //при переходе на новую строчку всякий раз надо будет смещать первый шестиугольник в ряду на такой шаг вниз.
+	void render_Global_Screen(sf::RenderTarget& renderer);
+	void Find_The_Chosen_Hex(float x, float y);
+	void render_The_ChosenHex(sf::RenderTarget& renderer);
+	bool Is_A_Hex_Active(); //Метод вызова приватного поля понадобится в конечном автомате.
 
-		for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-
-				float CurrentHex_Center_X = 30 - (i % 2) * x_offset + j * (2 * radius * cos(30 * pi / 180.f) - 1);
-				float CurrentHex_Center_Y = 30 + i * y_offset;
-
-				if ((abs(x - CurrentHex_Center_X) <= radius*cos(30 * pi / 180.f)) &&
-					(abs(y - CurrentHex_Center_Y) <= radius - abs(x - CurrentHex_Center_X) / sqrt(3)))
-				{
-					Chosen_Hex = sf::Vector2i(j, i); //Именно в таком порядке, так как j - номер столбца в матрице, сиречь координата по X.
-				}
-			}
-		}
-		return;
-	}
 };
