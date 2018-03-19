abdb6edb1c1d965b92643646e6cf58062ef9cb01
Serafim <s.grubas@g.nsu.ru>
2018-03-19 22:18:40 +0700

    clear "update"

--- StrategyGame/Student.cpp
+++ StrategyGame/Student.cpp
@@ -8,39 +8,11 @@ class WorldController;
 
 void Student::render(sf::RenderTarget& r)
 {
-	
+
 }
 
 
 void Student::update()
 {
-	
-	/*
-	for (int i = 0; i < 10000000; i++)
-	{
-		i++;
-	}
-		{
-			pos.x = pos.x + 1;
-			pos.y = pos.y + 1;
-		}
-		std::shared_ptr<Student> subj;
-		auto Ord = std::make_shared<WorldController>();
-	sf::Vector2u endPosition = { 5,1 };
-	std::vector<sf::Vector2i> VectorOfElementalMovement = Student::ABMovement(endPosition);
-	
-	for (int i = 0; i < VectorOfElementalMovement.size(); i++)
-	{
-		
-		auto some = Ord->move(subj,VectorOfElementalMovement[i]);
-
-		if (some ==true)
-		{
-			VectorOfElementalMovement.erase(VectorOfElementalMovement.begin() + i);
-			i--; // deleted element, elements shifted
-		}
-		
-	}
-	*/
 
 }
