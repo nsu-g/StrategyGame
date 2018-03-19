f22a26aa8d304edd4efddc6d1acca16f8aa88e98
Serafim <s.grubas@g.nsu.ru>
2018-03-19 22:20:25 +0700

    Attempts to make method for saving world

--- StrategyGame/WorldModel.cpp
+++ StrategyGame/WorldModel.cpp
@@ -1,4 +1,5 @@
 #include "WorldModel.h"
+#include <fstream>
 // Initialize model with zero
 WorldModel* WorldModel::WORLD = nullptr;
 
@@ -14,7 +15,16 @@ void WorldModel::SaveLevel(std::string filename)
 {
 	auto& actors = WorldModel::getWorldInstance()->actors;
 	auto& landscape = WorldModel::getWorldInstance()->landscape;
-
+	std::ofstream fout("H:\\" + filename + ".txt");
+	fout << "Class" << '\t' << "global_pos" << '\t';
+	fout << "local_pos" << '\n';
+	for (int i = 0; actors.size(); i++)
+	{
+		fout << "Actor" << '\t' << actors[i]->position() << '\t';
+		fout << actors[i]->loc_position() << '\t';
+		fout << actors[i]->health() << '\t' << actors[i]->faith() << '\t';
+		fout << actors[i]->get_power() << '\n';
+	} 
 }
 
 WorldModel* WorldModel::getWorldInstance()
