93800efc7d92d6063bb70c7e8c580c93387a6f13
SerafimGrubas <s.grubas@g.nsu.ru>
2018-03-20 12:11:17 +0400

    Убраны условия на ретурн

--- StrategyGame/City.cpp
+++ StrategyGame/City.cpp
@@ -13,17 +13,17 @@ void City::update()
 
 std::vector<int> City::dwellers()
 {
-	if (pick) return this->dw;
+	return this->dw;
 }
 
 float City::faith()
 {
-	if (pick) return this->relig;
+	return this->relig;
 }
 
 std::vector<float> City::kill()
 {
-	if (pick) return this->murd;
+	return this->murd;
 }
 
 void City::pick_up()
--- StrategyGame/City.h
+++ StrategyGame/City.h
@@ -7,8 +7,8 @@
 class City : public GameObject {
 	
 	std::vector<int> dw;
-	float relig; // вера в процентах
-	std::vector<float> murd; // (для инквизитора, для еретиков) или наоборот
+	float relig; 
+	std::vector<float> murd; 
 	bool pick;
 
 public:
@@ -16,11 +16,6 @@ public:
 
 	virtual void update() override;
 
-	virtual sf::Vector2u& position() override
-
-	{
-		return pos;
-	}
 	
 	std::vector<int> dwellers();
 	
