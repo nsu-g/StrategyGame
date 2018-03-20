8f1ed2451b10e7bd59a6093a04b610f5d5786c63
SerafimGrubas <s.grubas@g.nsu.ru>
2018-03-20 12:12:46 +0400

    Зачатки кода для SaveWorld()

--- StrategyGame/WorldModel.cpp
+++ StrategyGame/WorldModel.cpp
@@ -5,7 +5,7 @@ WorldModel* WorldModel::WORLD = nullptr;
 
 WorldModel::WorldModel()
 {
-	auto ps = std::make_shared<Actors>();
+	auto ps = std::make_shared<Student>();
 	actors.push_back(ps);
 	orders.push_back(std::make_shared<MoveOrder>(ps, sf::Vector2u(5,1)));
 }
@@ -13,18 +13,18 @@ WorldModel::WorldModel()
 
 void WorldModel::SaveLevel(std::string filename)
 {
-// 	auto& actors = WorldModel::getWorldInstance()->actors;
-// 	auto& landscape = WorldModel::getWorldInstance()->landscape;
-// 	std::ofstream fout("H:\\" + filename + ".txt");
-// 	fout << "Class" << '\t' << "global_pos" << '\t';
-// 	fout << "local_pos" << '\n';
-// 	for (int i = 0; actors.size(); i++)
-// 	{
-// 		fout << "Actor" << '\t' << actors[i]->position() << '\t';
-// 		fout << actors[i]->loc_position() << '\t';
-// 		fout << actors[i]->health() << '\t' << actors[i]->faith() << '\t';
-// 		fout << actors[i]->get_power() << '\n';
-// 	} 
+ 	auto& actors = WorldModel::getWorldInstance()->actors;
+ 	auto& landscape = WorldModel::getWorldInstance()->landscape;
+ 	std::ofstream fout("H:/" + filename + ".txt");
+ 	for (int i = 0; actors.size(); i++)
+ 	{
+		//actors[i]->save_me(fout);
+ 	}
+	for (int i = 0; landscape.size(); i++)
+	{
+		//landscape[i]->save_me(fout);
+	}
+	fout.close();
 }
 
 WorldModel* WorldModel::getWorldInstance()
