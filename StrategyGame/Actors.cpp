dc32fe6cc55630eb35655f4979fcc4d1a51bfa69
Serafim <s.grubas@g.nsu.ru>
2018-04-09 21:41:33 +0700

    Перенес поле loc_pos в gameobject и реализовал там метод loc_position

--- StrategyGame/Actors.cpp
+++ StrategyGame/Actors.cpp
@@ -43,23 +43,6 @@ void Actors::check_max()
 	if (this->relig > 100) this->relig = 100;
 }
 
-sf::Vector2u& Actors::loc_position()
-{
-	auto& world = WorldModel::getWorldInstance()->landscape;
-	for (int i = 0; i < world.size(); i++)
-	{
-		if (world[i]->position() == this->position())
-		{
-			City* pf = dynamic_cast<City*>(&(*(world[i])));
-			if (pf != nullptr)
-			{
-				if (this->position() == pf->position()) return this->loc_pos;
-			}
-		}
-	}
-	return this->position();
-}
-
 int& Actors::get_power()
 {
 	return this->power;
