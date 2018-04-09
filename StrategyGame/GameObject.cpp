14dccd4a58c2f4ab866ba704700e51123a055aab
Serafim <s.grubas@g.nsu.ru>
2018-04-09 23:40:37 +0700

    Добавлен общий метод save_me для всях объектов, который будут вызывать обеъкты если им нужо будет себя сохранить. Последнее не реализовано. Распространение парадигмы, что в .h файлах только объявление функций, а в .cpp их реализация

--- StrategyGame/GameObject.cpp
+++ StrategyGame/GameObject.cpp
@@ -1,6 +1,12 @@
 #include "GameObject.h"
 #include "City.h"
+#include <fstream>
 class City;
+
+sf::Vector2u& GameObject::position()
+{
+	return pos;
+}
 
 sf::Vector2u & GameObject::loc_position()
 {
@@ -17,4 +23,31 @@ sf::Vector2u & GameObject::loc_position()
 		}
 	}
 	return this->position();
+}
+
+sf::Sprite& GameObject::get_sprite()
+{
+	return sprite;
+}
+
+sf::Texture& GameObject::get_texture()
+{
+	return texture;
+}
+
+bool GameObject::get_active()
+{
+	return active;
+}
+
+void GameObject::set_active(bool new_active)
+{
+	this->active = new_active;
+}
+
+void GameObject::save_me(std::ofstream fout, std::string type_object)
+{
+	fout << type_object << '\t';
+	fout << this->position().x + ' ' + this->position().y << '\t';
+	fout << this->loc_position().x + ' ' + this->loc_position().y << '\t';
 }
--- StrategyGame/GameObject.h
+++ StrategyGame/GameObject.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <fstream>
 #include <SFML\Graphics.hpp>
 
 //Abstract class which will be parent to other objects, has position, renders to image, updates when world changes
@@ -16,31 +16,17 @@ public:
 	virtual void render(sf::RenderTarget&) = 0;
 	virtual void update() = 0;
 	
-	virtual sf::Vector2u& position()
-	{
-		return pos;
-	}
-	
+	virtual sf::Vector2u& position();
+
 	virtual sf::Vector2u& loc_position();
 	
+	virtual sf::Sprite& get_sprite();
 
-	virtual sf::Sprite& get_sprite()
-	{
-		return sprite;
-	}
-	
-	virtual sf::Texture& get_texture()
-	{
-		return texture;
-	}
-	
-	virtual bool get_active()
-	{
-		return active;
-	}
+	virtual sf::Texture& get_texture();
 
-	virtual void set_active(bool new_active)
-	{
-		this->active = new_active;
-	}
+	virtual bool get_active();
+
+	virtual void set_active(bool new_active);
+
+	virtual void save_me(std::ofstream fout, std::string type_object);
 };
